e362aca016d88a6cdaa39c4b2fe68a07e2718d2f
Rashi Abramson <roshkins@gmail.com>
2014-05-04 14:41:46 -0700

    Finished?

--- Average.rb
+++ Average.rb
@@ -2,18 +2,31 @@ require 'gosu'
 
 module Draw
 
-def draw(window,meditation,focus_array)
-   #level: low, med, hi
-   #symbol: med avg- calm, lo, med, hi
-   # display background based on symbol
-   load_image(meditation.to_s)
-   
+def draw_it(ms_percent)
+  #level: low, med, hi
+  #symbol: med avg- calm, lo, med, hi
+  # display background based on symbol
+  
+  @imgs[0].draw(0, ms_percent* self.height, 0 )
+  @imgs[1].draw(0, (ms_percent) * self.height - @imgs[1].height, 0 )
+
+  @boats_hash.each do |boat|
+    p "x:#{boat[:x]}"
+    p "y:#{self.height / 2 - boat[:img].height / 2}"
+    boat[:img].draw(boat[:x], self.height / 2 - boat[:img].height / 2, 1)
+  end
 end
 
 def load_image(folder)
    #random generate number to pick random image
    filepath = "Images/#{folder}/*.*";
-   @background_image = Gosu::Image.new(self,
+   Gosu::Image.new(self,
+      File.expand_path(Dir.glob(filepath).sample), true)
+end
+
+def load_boat(folder)
+  filepath = "Images/#{folder}/*.*";
+   Gosu::Image.new(self,
       File.expand_path(Dir.glob(filepath).sample), true)
 end
 
--- main.rb
+++ main.rb
@@ -16,34 +16,42 @@ class GameWindow < Gosu::Window
       exit
     end
 
-    @read_sockets = []
-    @background_image = Gosu::Image.new(self, 
-      File.expand_path("Images/Calm/noaa_boats_calm_fish0581.jpg"), true)
+    @read_sockets_med = []
+    @read_sockets_focus = []
+    
     @pids = []
     if ARGF.argv.length > 0
       ARGF.argv.each do |ipandport|
         ip, port = *(ipandport.split(":"))
         port = true if (port == "true" || port == "1726")
         port = false if (port == "false" || port == "3008" || port.nil?)
-        write, read = Socket.pair(:UNIX, :DGRAM, 0)
-        @read_sockets << read
+            writer, reader = Socket.pair(:UNIX, :DGRAM, 0)
+            writer2,  reader2 = Socket.pair(:UNIX, :DGRAM, 0)
+            @read_sockets_med << reader
+            @read_sockets_focus << reader2
         @pids.push(fork do
+            reader.close
+            reader2.close
           Signal.trap("INT"){exit}
           EmotivSDK::Engine.new(ip, port) do |event|
-            write.puts "meditation:(#{event.meditation_score},1)"
-            false
+            writer.puts "meditation:(#{event.meditation_score},1)"
+            writer2.puts "focus:(#{event.cognitiv_action == :COG_LIFT ? event.cognitiv_action_power : 0 },1)"
+              false
           end
           ""
         end)
       end
     else
-      write, read = Socket.pair(:UNIX, :DGRAM, 0)
-      @read_sockets << read
+      #write, read = Socket.pair(:UNIX, :DGRAM, 0)
+      #@read_sockets << read
       @pids.push(fork do
         Signal.trap("INT"){exit}
         EmotivSDK::Engine.new("127.0.0.1", true) do |event|
-            write.puts "meditation:(#{event.meditation_score},1)"
-            write.puts "focus:(#{event.cognitiv_action == :COG_LIFT ? event.cognitiv_action_power : 0 },1)"
+          reader, writer = IO.pipe
+            @read_sockets << reader
+            writer.puts "meditation:(#{event.meditation_score},1)"
+            writer.puts "focus:(#{event.cognitiv_action == :COG_LIFT ? event.cognitiv_action_power : 0 },1)"
+            writer.close
             false
           end
         ""
@@ -54,8 +62,8 @@ class GameWindow < Gosu::Window
   def update
     med_total = 0
     med_count = 0
-    @read_sockets.each do |socket|
-    response = socket.gets
+    @read_sockets_med.each do |socket|
+      response = socket.gets
       if /meditation:\(([0-9.]+),(\d+)\)/ =~ response
         my_match = response.match(/meditation:\(([0-9.]+),(\d+)\)/)
         med_total += my_match[1].to_f
@@ -76,22 +84,52 @@ class GameWindow < Gosu::Window
     #puts "Meditation Avg: #{med_total/med_count}" 
     
     @focus_array = []
-    @read_sockets.each do |socket|
-    response = socket.gets
+    @read_sockets_focus.each do |socket|
+      
+    begin
+      response = socket.gets
+    rescue
+      response = "focus:(0.0,1)"
+    end
+    
       if /focus:\(([0-9.]+),(\d+)\)/ =~ response
         my_match = response.match(/focus:\(([0-9.]+),(\d+)\)/)
-        focus_array << my_match[1].to_f
+        @focus_array << my_match[1].to_f
       end
     end
-    #puts "Focus Avg: #{focus_total/focus_count}" 
+    puts "Focus: #{@focus_array}" 
+    @last ||= 0
+    @ms_percent = (Gosu::milliseconds - @last) / 4000.0
+    p (Gosu::milliseconds - @last) 
+    @imgs ||= [load_image(@med_symb.to_s)] * 2
+    if (((@ms_percent) * self.height  - @imgs[1].height) > self.height) 
+      img = load_image(@med_symb.to_s)
+      @imgs.pop
+      @imgs .unshift img
+      @last = Gosu::milliseconds
+    end
+    
+    
+    
+    #boats
+    
+    @boats ||= [load_boat("boats")] * @focus_array.length
+    @boats_hash = @boats.map.with_index do |img, i|
+      descrete_part = (self.width/@focus_array.length)
+      
+      p amplitude = (descrete_part/2.0) * (1.0-@focus_array[i])
+      p my_sin = Math.sin((Gosu::milliseconds/1000.0) * 2.0 * Math::PI)
+      {img: img, x: (( amplitude * my_sin + descrete_part * 1.5 + (i-1) * descrete_part))}
+ 
+    end
     true
   end
   
   def draw
-    @background_image.draw(0,0,0)
-    super self, @med_symb, @focus_array
+    draw_it @ms_percent
   end
   
+
 end
 
 window = GameWindow.new
