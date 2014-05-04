0b1d7a03096a09d5ebc2aa3e2faeb45eb23ca2e9
Rod <run2rod@gmail.com>
2014-05-04 11:02:01 -0700

    update to Average

--- Average.rb
+++ Average.rb
@@ -2,20 +2,19 @@ require 'gosu'
 
 module Draw
 
-def update(meditation,focus_array)
+def draw(self,meditation,focus_array)
    #level: low, med, hi
    #symbol: med avg- calm, lo, med, hi
    # display background based on symbol
-   case meditation
-   when :Calm
-      #load image from Calm file
-   when :Mild
-      #load image from Mild file
-   when :Moderate
-      #load image from Moderate file
-   when :Rocky
-      #load image from Rocky file
-   end
+   load_image(meditation.to_s)
+   
+end
+
+def load_image(folder)
+   #random generate number to pick random image
+   filepath = "Images/" + folder;
+   @background_image = Gosu::Image.new(self,
+      File.expand_path(filepath), true)
 end
 
 end
